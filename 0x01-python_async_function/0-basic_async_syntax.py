02fcb6cf3f44871b448b0f753c4163b10a0dbc64
Brian Omondi <omondiibrian00@gmail.com>
2024-01-15 23:07:09 +0300

    asynchrounous code with py

---
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+""" Imported modules
+asyncio, random
+"""
+import asyncio
+import random
+
+
+async def wait_random(max_delay: int = 10) -> float:
+    """ Takes in an integer argument, waits for a random delay between 0 and max_delay
+    and eventually returns it.
+    """
+    i = random.uniform(0, max_delay)
+    await asyncio.sleep(i)
+    return i
