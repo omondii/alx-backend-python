fdc39d63d2904e941f42a3b45b989ee93da8a636
Brian Omondi <omondiibrian00@gmail.com>
2024-01-16 00:33:50 +0300

    req: fix

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -7,8 +7,8 @@ import random
 
 
 async def wait_random(max_delay: int = 10) -> float:
-    """ Takes in an integer argument, waits for a random delay between 0 and max_delay
-    and eventually returns it.
+    """ Takes in an integer argument, waits for a random delay between 0
+    and max_delay and eventually returns it.
     """
     i = random.uniform(0, max_delay)
     await asyncio.sleep(i)
--- 0x01-python_async_function/2-measure_runtime.py
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -8,11 +8,12 @@ asyncio, wait_n
 
 
 async def measure_time(n: int, max_delay: int) -> float:
-    """ measure_time function with integers n and max_delay as arguments that measures
-    the total execution time for wait_n(n, max_delay)"""
-    start = time.time()
+    """ measure_time function with integers n and max_delay as
+    arguments that measures the total execution time for
+    wait_n(n, max_delay)"""
+    s = time.perf_counter()
     await wait_n(n, max_delay)
-    end = time.time()
+    elapsed = time.perf_counter() - s
+    avarage_time = elapsed / n
 
-    totaltime = end - start
-    return totaltime / n
+    return avarage_time
