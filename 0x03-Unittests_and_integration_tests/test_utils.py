7eae84eb8d5e1ad236129a893dd8c84c20c46f33
Brian Omondi <omondiibrian00@gmail.com>
2024-04-05 10:57:03 +0300

    Add test_client

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -1,35 +1,44 @@
 #!/usr/bin/env python3
-""" Utils.py """
+"""A module for testing the utils module.
+"""
 import unittest
-from utils import access_nested_map
-from typing import Dict, Union, Tuple
-from parameterized import parameterized, parameterized_class
-from unittest.mock import patch, mock
+from typing import Dict, Tuple, Union
+from unittest.mock import patch, Mock
+from parameterized import parameterized
+
+from utils import (
+    access_nested_map,
+    get_json,
+    memoize,
+)
 
 
 class TestAccessNestedMap(unittest.TestCase):
-    """ Implement the TestAccessNestedMap.test_access_nested_map method
-    to test that the method returns what it is supposed to. """
+    """Tests the `access_nested_map` function."""
     @parameterized.expand([
         ({"a": 1}, ("a",), 1),
         ({"a": {"b": 2}}, ("a",), {"b": 2}),
         ({"a": {"b": 2}}, ("a", "b"), 2),
     ])
-    def test_access_nested_map(self, nested_map: Dict, path: Tuple[str],
-                               expected: Union[Dict, int]) -> None:
-        """ Tests that the method returns what it is supposed to. """
+    def test_access_nested_map(
+            self,
+            nested_map: Dict,
+            path: Tuple[str],
+            expected: Union[Dict, int],
+            ) -> None:
+        """Tests `access_nested_map`'s output."""
         self.assertEqual(access_nested_map(nested_map, path), expected)
 
-        @parameterized.expand([
-            ({}, ("a",), KeyError),
-            ({"a": 1}, ("a", "b"), KeyError),
-        ])
+    @parameterized.expand([
+        ({}, ("a",), KeyError),
+        ({"a": 1}, ("a", "b"), KeyError),
+    ])
     def test_access_nested_map_exception(
             self,
             nested_map: Dict,
             path: Tuple[str],
             exception: Exception,
-    ) -> None:
+            ) -> None:
         """Tests `access_nested_map`'s exception raising."""
         with self.assertRaises(exception):
             access_nested_map(nested_map, path)
@@ -45,7 +54,7 @@ class TestGetJson(unittest.TestCase):
             self,
             test_url: str,
             test_payload: Dict,
-    ) -> None:
+            ) -> None:
         """Tests `get_json`'s output."""
         attrs = {'json.return_value': test_payload}
         with patch("requests.get", return_value=Mock(**attrs)) as req_get:
@@ -55,7 +64,6 @@ class TestGetJson(unittest.TestCase):
 
 class TestMemoize(unittest.TestCase):
     """Tests the `memoize` function."""
-
     def test_memoize(self) -> None:
         """Tests `memoize`'s output."""
         class TestClass:
@@ -69,7 +77,7 @@ class TestMemoize(unittest.TestCase):
                 TestClass,
                 "a_method",
                 return_value=lambda: 42,
-        ) as memo_fxn:
+                ) as memo_fxn:
             test_class = TestClass()
             self.assertEqual(test_class.a_property(), 42)
             self.assertEqual(test_class.a_property(), 42)
